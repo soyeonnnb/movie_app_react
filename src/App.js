444ed33a96ce5f722155ac2143eefe9ea0772c2c
soyeonnnb <la28s5d@naver.com>
2021-09-14 22:52:19 +0900

    ~#2.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+function Food({fav}){
+  console.log(fav)
+  return (
+    <h1>I like {fav}</h1>
+  )
+}
+
+
 function App() {
   return (
-    <div className="App">
+    <div>
+      <h1>Hello</h1>
+      <Food fav="치킨" />
+      <Food fav="라면" />
+      <Food fav="삼겹살" />
+      <Food fav="족발" />
     </div>
   );
 }
